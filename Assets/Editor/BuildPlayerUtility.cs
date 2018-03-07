20087d2e37c9d82b9677564f59f7d3322f4a306f
pilingyan <463668874@qq.com>
2018-03-07 15:14:24 +0800

    add BuildPlayerUtility.

---
+++ Assets/Editor/BuildPlayerUtility.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class BuildPlayerUtility : MonoBehaviour {
+
+// <summary>  
+/// 获取当前本地时间戳  
+/// </summary>  
+/// <returns></returns>        
+public static long GetCurrentTimeUnix()  
+{  
+    TimeSpan cha = (DateTime.Now - TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)));  
+    long t = (long)cha.TotalSeconds;  
+    return t;  
+}  
+
+	public static void BuildAndroid(){
+		long startTime = GetCurrentTimeUnix();	
+		BuildPipeline.BuildPlayer(new BuildPlayerOptions(){
+			 scenes = new[]{"Assets/launcher.unity"},
+			  locationPathName = "target",
+			   target = BuildTarget.StandaloneWindows,
+			    options = BuildOptions.None});
+
+		long wasteSeconds = GetCurrentTimeUnix() - startTime;
+		Debug.LogFormat("waste seconds:{0}s", wasteSeconds);	
+
+	}
+}
